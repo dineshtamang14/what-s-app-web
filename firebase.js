b5431d46a494d67e23709d622aed63db14f79435
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-01-31 17:45:51 +0530

    initial commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAuQPvtWmoevthz_ipToRAHM_Jz04sCEvc",
+    authDomain: "what-s-app-ff53c.firebaseapp.com",
+    projectId: "what-s-app-ff53c",
+    storageBucket: "what-s-app-ff53c.appspot.com",
+    messagingSenderId: "899838841901",
+    appId: "1:899838841901:web:8840a45c33eb981afbd4a4"
+  };
