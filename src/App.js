18c2bb21fdfad00f8b3f198ab551c46332588eee
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-01-31 19:07:31 +0530

    create  components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Chat from './Chat';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="App">
       <h1>lets build whatsapp</h1>
+
+      {/* Siderbar */}
+      <Sidebar />
+
+      {/* Chat component */}
+      <Chat />
     </div>
   );
 }
